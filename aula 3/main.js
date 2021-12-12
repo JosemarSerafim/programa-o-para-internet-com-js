7c06603cecfa2a37f00eb73968150c6ae52de07c
josemar serafim <josemarserafim2016@gmail.com>
2021-12-12 20:34:25 +0100

    aula 3

---
+++ aula 3/main.js
@@ -0,0 +1,10 @@
+// var idade =Number(prompt('Digite sua idade:'));
+
+// if(idade >= 18){
+//     alert('Maior de Idade');
+// } else {
+//     alert("Menor de idade");
+// }
+
+var d = new Date();
+alert(d.getDate() + "/" + (d.getMonth()+1));
