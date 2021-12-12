00ce17726f6f61ba5f275af47debb46f105de14d
josemar serafim <josemarserafim2016@gmail.com>
2021-12-12 19:55:10 +0100

    aula 2

---
+++ aula 2/main.js
@@ -0,0 +1,18 @@
+var lista = ["maça", "pera", "laranja"];
+//push adiciona como ultimo elemento da lista
+//lista.push("uva");
+
+//pop - retira o último eçlemento da lista
+//lista.pop()
+
+//reverse() - tráz a lista de forma contrária
+//alert(lista.reverse()); 
+
+//join() - transforma em string e junta
+// alert(lista.join("-"));
+
+var fruta = {
+    nome: 'maça',
+    cor:'vermelha'
+}
+
